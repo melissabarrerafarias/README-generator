27803a4c92d516cd73dece9f54b8dc9a48190e66
melissikiss <melissikiss@gmail.com>
2020-11-12 13:04:53 -0800

    created basic README layout

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,6 +2,45 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description 
+  ${data.description}
+
+
+  ## Table of Contents
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[License](#license)
+  *[Contributing](#contributing)
+  *[Tests](#test)
+  *[Questions](#questions)
+
+
+  ## Installation
+  ${data.installation}
+
+
+  ## Usage
+  ${data.usage}
+
+
+  ## License 
+  ${data.license}
+
+
+  ## Contributing 
+  ${data.contribution}
+
+
+  ## Tests 
+  ${data.tests}
+
+
+  ## Questions 
+  Github Username : ${data.username}
+  [Link to Github Profile](https://github.com/${data.username})
+
+  If you have any further questions or curiosities, feel free to contact me through email at ${data.email}!
+
 `;
 } 
 
